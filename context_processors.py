77382128db1e9785dfc5eeadd637e87e4d63b6ba
Salma Khodaei <khodaeisalma8@gmail.com>
2025-08-28 14:41:34 -0700

    fix css issue

--- context_processors.py
+++
@@ -1,4 +0,0 @@
-
-# available in all template files, always returns a dictionary
-def categories(request):
-    return {"categories": ["Movies"]}
---
+++ context_processors/menu_categories.py
@@ -0,0 +1,4 @@
+
+# available in all template files, always returns a dictionary
+def menu_categories(request):
+    return {"menu_categories": ["Movies"]}
